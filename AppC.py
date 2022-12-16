f584126e1c3c72d8497cb231570f9aad91b6c448
Camilo <camiloariasabad@gmail.com>
2022-12-16 15:36:01 +0100

    [experimentC] getting there

--- AppC.py
+++ AppC.py
@@ -28,8 +28,9 @@ initial_df["day"]=pd.to_datetime(initial_df["day"])
 app.layout = html.Div(children=[
                 html.Div(children=[
                     html.Div(children=[
-                        html.H1('CYTOPATHOLOGY MONITOR',style={"margin-top":"10px"}),
-                        html.Img(src="assets/medicine.svg", style={'height': '40px','margin-bottom': '10px','margin-left':'20px'})],
+                        html.H1('CYTOPATHOLOGY MONITOR',style={"margin-top":"10px","font-size":20}),
+                        html.Img(src="assets/medicine.svg", style={'height': '40px','margin-bottom': '10px','margin-left':'20px'})
+                        ],
                         style={"display":"table"}
                     ),
                     html.Div(children=[
@@ -37,50 +38,17 @@ app.layout = html.Div(children=[
                         html.Label('Time period:',style={'font-size':"20px",'font-weight':"bold","display":"flex"}),
                         dcc.Dropdown(['last week', 'last month', 'last year'], 'last month',
                             style={'width': '200px','color':'black',"font-size":"20px","display":"flex","align-items": "right","justifyContent":"right"} ,id="default-time-ranges"),
-                    # dcc.DatePickerRange(
-                        #     id = 'date-range',
-                        #     start_date_placeholder_text = last_month_date,
-                        #     end_date = date.today(),
-                        #     max_date_allowed = date.today(),
-                        #     day_size=40
-                        # ),
-                    #html.H3('Type elalskjdf selected: ', style={'fontWeigth': 'bold','textAlign':'center','font-size':"30px"},id='type-selected'),
-                            #html.Div(id='type-selected',style={'margin': '0px'}),
-                        # dcc.Graph(
-                        #     id='tree-map',
-                        #     figure={},
-                        #     clickData={},
-                        #     config={
-                        #     'staticPlot': False,     # True, False
-                        #     'scrollZoom': True,      # True, False
-                        #     'doubleClick': 'reset',  # 'reset', 'autosize' or 'reset+autosize', False
-                        #     'showTips': True,       # True, False
-                        #     'displayModeBar': None,  # True, False, 'hover'
-                        #     'watermark': False,
-                        #     # 'modeBarButtonsToRemove': ['pan2d','select2d'],
-                        #     }
-                        #     #,style={"height":"40px"}
-                        #         )
-                            #'align-items': 'flex-start'
-                        #html.H3('Type elalskjdf selected: ', style={'fontWeigth': 'bold','textAlign':'center','font-size':"30px"},id='type-selected'),
-                            #html.Div(id='type-selected',style={'margin': '0px'}),
-                        # dcc.Graph(
-                        #     id='tree-map',
-                        #     figure={},
-                        #     clickData={},
-                        #     config={
-                        #     'staticPlot': False,     # True, False
-                        #     'scrollZoom': True,      # True, False
-                        #     'doubleClick': 'reset',  # 'reset', 'autosize' or 'reset+autosize', False
-                        #     'showTips': True,       # True, False
-                        #     'displayModeBar': None,  # True, False, 'hover'
-                        #     'watermark': False,
-                        #     # 'modeBarButtonsToRemove': ['pan2d','select2d'],
-                        #     }
-                        #     #,style={"height":"40px"}
-                        #         )
-                            ], style={"margin-left":"20mm","display":'table','justifyContent': 'center', 'align-items': 'center','width': '40%',"height":"6cm"}
+
+
+                            ], style={"margin-left":"20mm","display":'table','justifyContent': 'center', 'align-items': 'center','width': '40%'}
                     ),
+                    dcc.DatePickerRange(
+                            id = 'date-range',
+                            start_date_placeholder_text = last_month_date,
+                            end_date = date.today(),
+                            max_date_allowed = date.today(),
+                            day_size=40,
+                        style={"width":"10cm"}),
                     html.Div(children=[
                         html.H3('Type : ', style={'fontWeigth': 'bold','textAlign':'center','font-size':"20px"},id='type-selected'),
                         dcc.Graph(
@@ -102,7 +70,7 @@ app.layout = html.Div(children=[
                                     style={"display":"table"})
 
                     ],
-                    style={"display":"flex",'textAlign': 'center','height':'40mm','font-size':"20px",'justifyContent': 'left'},
+                    style={"height":"40px","display":"flex","textAlign": "center","height":"30mm","font-size":"20px","justifyContent":"center"},
                     id="grad"
                     ),
                 html.Div(children=[
@@ -171,8 +139,16 @@ app.layout = html.Div(children=[
 
                         ], style={'padding':'10px', 'border':None}),
 
+
                         #Fourth GRAPH
                         html.Div(children=[
+                            #html.H2('MVP', style={'textAlign': 'center'}),
+                            html.H3('Result selected: ', style={'fontWeigth': 'bold'}),
+                             dcc.RadioItems(genotype_list,
+                            value=genotype_list[0],
+                            id = 'genotype-radio'
+                            ),
+                            html.Div(id='result-selected'),
                             dcc.Graph(id='mvp-graph', figure={},
                                 config={
                                     'staticPlot': False,     # True, False
@@ -215,42 +191,42 @@ app.layout = html.Div(children=[
 
 
 
-# @app.callback(
-#     Output(component_id= 'date-range', component_property='start_date'),
-#     Output(component_id= 'date-range', component_property='end_date'),
-#     Input(component_id='default-time-ranges', component_property='value')
-# )
-# def update_time_range(input_range):
-#     """Control time range selection."""
-#     if input_range == 'last week':
-#         start_date = last_week_date
-#     elif input_range == 'last month':
-#         start_date =last_month_date
-#     elif input_range == 'last year':
-#         start_date = last_year_date
-#     end_date = date.today()
-#     return start_date, end_date
+@app.callback(
+    Output(component_id= 'date-range', component_property='start_date'),
+    Output(component_id= 'date-range', component_property='end_date'),
+    Input(component_id='default-time-ranges', component_property='value')
+)
+def update_time_range(input_range):
+    """Control time range selection."""
+    if input_range == 'last week':
+        start_date = last_week_date
+    elif input_range == 'last month':
+        start_date =last_month_date
+    elif input_range == 'last year':
+        start_date = last_year_date
+    end_date = date.today()
+    return start_date, end_date
 
 
 
-# @app.callback(
-#     #Output(component_id='type-selected', component_property='children'),
-#     #Output(component_id='adequacy-selected', component_property='children'),
-#     Output(component_id='result-selected', component_property='children'),
-#     Output(component_id='genotype-selected', component_property='children'),
-#     Output(component_id='types-graph', component_property='figure'),
-#     Output(component_id='tree-map', component_property='figure'),
-#     Output(component_id='results-graph', component_property='figure'),
-#     Output(component_id='mvp-graph', component_property='figure'),
-#     Output(component_id='qc-graph', component_property='figure'),
+@app.callback(
+    #Output(component_id='type-selected', component_property='children'),
+    #Output(component_id='adequacy-selected', component_property='children'),
+    Output(component_id='result-selected', component_property='children'),
+    Output(component_id='genotype-selected', component_property='children'),
+    Output(component_id='types-graph', component_property='figure'),
+    Output(component_id='tree-map', component_property='figure'),
+    Output(component_id='results-graph', component_property='figure'),
+    Output(component_id='mvp-graph', component_property='figure'),
+    Output(component_id='qc-graph', component_property='figure'),
 
-#     Input(component_id = 'type-dropdown', component_property='value'),
-#     Input(component_id='tree-map', component_property='clickData'),
-#     Input(component_id='selected-result', component_property='value'),
-#     Input(component_id='genotype-radio', component_property='value'),
-#     # Input(component_id='default-time-range', component_property='start_date'),
-#     Input(component_id='default-time-ranges', component_property='value')
-#)
+    Input(component_id = 'type-dropdown', component_property='value'),
+    Input(component_id='tree-map', component_property='clickData'),
+    Input(component_id='selected-result', component_property='value'),
+    Input(component_id='genotype-radio', component_property='value'),
+    # Input(component_id='default-time-range', component_property='start_date'),
+    Input(component_id='default-time-ranges', component_property='value')
+)
 def update_graphs(type, click_data, result, genotype, input_range):
 
     """Return all graphs based on interactive filters."""
