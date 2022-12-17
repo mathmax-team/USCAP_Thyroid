675d8de4193351836e3157bc781e46802185dca8
Camilo <camiloariasabad@gmail.com>
2022-12-17 21:20:42 +0100

    [experimentC] better static version

--- Flexapp.py
+++ Flexapp.py
@@ -2,91 +2,276 @@
 import dash
 import dash_bootstrap_components as dbc
 from dash import html, dcc, Output, Input
+from sample_data import last_week_date, last_year_date, last_month_date,test_type, results_list, genotype_list
+from datetime import date
 
-items=['Last week', 'Last month', 'Last year']
-atems=list(map(lambda z: dbc.DropdownMenuItem(z,id=z),items))
 
-INP=list(map(lambda z: Input(z,"n_clicks"),items))
+################## MAKE DROPDOWN FROM LIST
+def make_drop(lista,id):
+    items=lista
+    atems=list(map(lambda z: dbc.DropdownMenuItem(z,id=z),items))
+    inputs=list(map(lambda z: Input(z,"n_clicks"),items))
 
-dropletter=dbc.DropdownMenu(atems,
-            label="Time period",
+    menu=dbc.DropdownMenu(atems,
+            label=lista[0],
             className="mb-3",
-            id= 'iddropdownmenu'  #create an id for the dropdown menu to be used in the graph callback to modify the label
-        ,color="warning"
+            id= id  #create an id for the dropdown menu to be used in the graph callback to modify the label
+        ,color="success",
+        size="sm"
         )
+    return {"drop":menu,"inputs":inputs,"list":lista}
 
 
-page_header=[dbc.Row(html.Div(style={"height":"10px"})),dbc.Row([
-                    dbc.Col(
-                    [dbc.Row(html.Img(src="assets/medicine.svg",style={'height': '60px'})),
-                        dbc.Row(html.H4('CYTOPATHOLOGY MONITOR'),style={"textAlign":"center"})
-                        ],
-                    width="3",
-                    align="end"),
+###################  GENERATE THE DROPDOWN ELEMENTS  #######################
 
-                    ##Time and default ranges
-                    dbc.Col(
-                        #html.Label('Time period:',style={'font-size':"20px",'font-weight':"bold","display":"flex"}),
-                        dropletter),
-                    ###DatePicker
-                    dbc.Col(
-                    dcc.DatePickerRange(
-                            id = 'date-range',
-                            #start_date_placeholder_text = last_month_date,
-                            #end_date = date.today(),
-                            #max_date_allowed = date.today(),
-                            #day_size=30,
-                        #style={"width":"10cm","height":"20cm","margin-top":"50px","margin-left":"20px"}
-                    ),width=4),
-                    ##Title and Treegraph
-                    dbc.Col([
-                        #dbc.Row(html.H3('Type : ', style={'fontWeigth': 'bold','textAlign':'center','font-size':"20px"},id='type-selected')),
-                        dbc.Row(dcc.Graph(
-                                id='tree-map',
-                                figure={},
-                                clickData={},
-                                config={
-                                'staticPlot': False,     # True, False
-                                'scrollZoom': True,      # True, False
-                                'doubleClick': 'reset',  # 'reset', 'autosize' or 'reset+autosize', False
-                                'showTips': True,       # True, False
-                                'displayModeBar': None,  # True, False, 'hover'
-                                'watermark': False,
-                                # 'modeBarButtonsToRemove': ['pan2d','select2d'],
-                                },
-                                style={"height":"50px","width":"300px"}
-                                    ),
-                                    )
-                                    ]
-                                    )
 
-                    ],
-                    #style={"display":"flex","height":"30mm"},
-                    id="grad",
-                    #align="center",
-                    justify="end",
-                    className="pad-row"
+dropletter=make_drop(['Last week', 'Last month', 'Last year'],"dropletter")
+type=make_drop(test_type,"type")
+weird=make_drop(results_list,"weird")
+genotype=make_drop(genotype_list,"genotype")
+mvp=make_drop(["A","B"],"mvp")
+
+###################### PAGE HEADER    #######################
+
+
+page_header=[
+    dbc.Row(html.Div(style={"height":"10px"})),
+    dbc.Row([
+        dbc.Col(
+            [dbc.Row(html.Img(src="assets/medicine.svg",style={'height': '60px'})),
+            dbc.Row(html.H3('Cytopathology Monitor'),style={"textAlign":"center"})
+            ],
+            #width="3",
+            align="end"),
+        dbc.Col(dropletter["drop"]),
+        dbc.Col(dcc.DatePickerRange(
+                id = 'date-range',
+                start_date_placeholder_text = last_month_date,
+                end_date = date.today(),
+                max_date_allowed = date.today(),
+                #day_size=30,
+                #style={"width":"10cm","height":"20cm","margin-top":"50px","margin-left":"20px"}
+                    ),
+                    width=4
+                ),
+        dbc.Col([
+            dbc.Row(dcc.Graph(
+                id='tree-map',
+                figure={},
+                clickData={},
+                config={
+                'staticPlot': False,     # True, False
+                'scrollZoom': True,      # True, False
+                'doubleClick': 'reset',  # 'reset', 'autosize' or 'reset+autosize', False
+                'showTips': True,       # True, False
+                'displayModeBar': None,  # True, False, 'hover'
+                'watermark': False,
+                # 'modeBarButtonsToRemove': ['pan2d','select2d'],
+                        },
+                    style={"height":"100px","width":"300px"}
+                                ),
                     )
-                    ]
+                                ],
+                )
+            ],
+            id="page_header",
+            justify="end",
+            )
+        ]
+#############################     Imgrid   ###################################################
+Imgrid=html.Div(children=[
+                  #First GRAPH
+                    html.Div(
+                        dbc.Col([
+                            dbc.Row(type["drop"],style={"textAlign":"center"}),
+                            dbc.Row(
+                            dcc.Graph(
+                                id='types-graph',
+                                figure={},
+                                config={
+                                    'staticPlot': False,     # True, False
+                                    'scrollZoom': True,      # True, False
+                                    'doubleClick': 'reset+autosize',  # 'reset', 'autosize' or 'reset+autosize', False
+                                    'showTips': False,       # True, False
+                                    'displayModeBar': False,  # True, False, 'hover'
+                                    'watermark': False,
+                                    # 'modeBarButtonsToRemove': ['pan2d','select2d'],
+                                    },
+                            style={"height":"300px"}
+                            ),
+                            )
+                        ],
+                        style={'padding':'5px'}
+                        )),
+                        #Second GRAPH
+                        # html.Div(children=[
+                        #     html.H2('Adequacy', style={'textAlign': 'center'}),
 
-# 2. Create a Dash app instance
+                        # ], style={'padding':'10px', 'border':None}),
+
+                        #Third GRAPH
+                        html.Div(
+                            dbc.Col([
+                                dbc.Row(weird["drop"],style={"textAlign":"center"}),
+                                dbc.Row(
+                                dcc.Graph(id='results-graph', figure={},
+                                    config={
+                                        'staticPlot': False,     # True, False
+                                        'scrollZoom': False,      # True, False
+                                        'doubleClick': 'reset',  # 'reset', 'autosize' or 'reset+autosize', False
+                                        'showTips': False,       # True, False
+                                        'displayModeBar': False,  # True, False, 'hover'
+                                        'watermark': False,
+                                        # 'modeBarButtonsToRemove': ['pan2d','select2d'],
+                                            },
+                                        style={"height":"300px"}
+                                        )
+                                )
+                        ],
+                        ),
+                        style={'padding':'5px'}
+                        ),
+                        html.Div(dbc.Col(
+                            [
+                            dbc.Row(genotype["drop"],style={"textAlign":"center"}),
+                            dbc.Row(
+                                dcc.Graph(id='mvp-graph', figure={},
+                                    config={
+                                        'staticPlot': False,     # True, False
+                                        'scrollZoom': False,      # True, False
+                                        'doubleClick': 'reset+autosize',  # 'reset', 'autosize' or 'reset+autosize', False
+                                        'showTips': False,       # True, False
+                                        'displayModeBar': False,  # True, False, 'hover'
+                                        'watermark': False,
+                                        # 'modeBarButtonsToRemove': ['pan2d','select2d'],
+                                            },
+                                        style={"height":"300px"}
+                                        ),
+                            )
+                            # dcc.Dropdown(genotype_list,
+                            # value=genotype_list[0],
+                            # id = 'selected-genotype'
+                            # ),
+                        ],
+                        style={'padding':'5px', 'border':None}
+                        ),
+                        ),
+
+                        #Fifth GRAPH
+                        html.Div(dbc.Col([
+                            dbc.Row(mvp["drop"],style={"textAlign":"center"}),
+                            dbc.Row(
+                                dcc.Graph(id='qc-graph', figure={},
+                                config={
+                                    'staticPlot': False,     # True, False
+                                    'scrollZoom': False,      # True, False
+                                    'doubleClick': 'reset',  # 'reset', 'autosize' or 'reset+autosize', False
+                                    'showTips': False,       # True, False
+                                    'displayModeBar': False,  # True, False, 'hover'
+                                    'watermark': False,
+                                    # 'modeBarButtonsToRemove': ['pan2d','select2d'],
+                                    },
+                                    style={"height":"300px"}
+                                    ),
+                            )
+                        ],
+                        style={'padding':'5px'}),
+                        )
+                        ],
+                        #html.Div('6', style={'padding':'0px', 'border':None}),
+                    id="grid",
+                    style={'display': 'grid', 'gridTemplateRows':"repeat(2, 1fr)",'gridTemplateColumns': 'repeat(2, 1fr)', 'gridAutoFlow': 'row',"height":"100px"}
+)
+
+
+######################################
+
 app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
-app.layout=dbc.Container(page_header,fluid="True")
+app.layout=dbc.Container([dbc.Row(page_header),dbc.Row(Imgrid)],fluid="True")
+############## UPDATE DROPDOWN
 
+
+########################### CALL BACK letterdrop ###################
 @app.callback(
     [
-     Output("iddropdownmenu", "label")
+     Output("dropletter", "label")
      ],
-    INP
+    dropletter["inputs"]
 )
-def make_graph(*args):
+def update_default_period(*args):
     ctx = dash.callback_context
-
     if not ctx.triggered:
-        button_id = "Time period"
+        button_id = dropletter["list"][0]
     else:
         button_id = ctx.triggered[0]["prop_id"].split(".")[0]
 
     return [button_id]
+########################### CALL BACK type ###################
+@app.callback(
+    [
+     Output("type", "label")
+     ],
+    type["inputs"]
+)
+def update_default_period(*args):
+    ctx = dash.callback_context
+    if not ctx.triggered:
+        button_id = type["list"][0]
+    else:
+        button_id = ctx.triggered[0]["prop_id"].split(".")[0]
+
+    return [button_id]
+################  CALL BACK weird #################
+
+@app.callback(
+    [
+     Output("weird", "label")
+     ],
+    weird["inputs"]
+)
+def update_default_period(*args):
+    ctx = dash.callback_context
+    if not ctx.triggered:
+        button_id = weird["list"][0]
+    else:
+        button_id = ctx.triggered[0]["prop_id"].split(".")[0]
+
+    return [button_id]
+
+########################## CALL BACK genotype ###################
+@app.callback(
+    [
+     Output("genotype", "label")
+     ],
+    genotype["inputs"]
+)
+def update_default_period(*args):
+    ctx = dash.callback_context
+    if not ctx.triggered:
+        button_id = genotype["list"][0]
+    else:
+        button_id = ctx.triggered[0]["prop_id"].split(".")[0]
+
+    return [button_id]
+################  CALL BACK weird #################
+
+@app.callback(
+    [
+     Output("mvp", "label")
+     ],
+    mvp["inputs"]
+)
+def update_default_period(*args):
+    ctx = dash.callback_context
+    if not ctx.triggered:
+        button_id = mvp["list"][0]
+    else:
+        button_id = ctx.triggered[0]["prop_id"].split(".")[0]
+
+    return [button_id]
+
+
+
+
 if __name__ == "__main__":
     app.run_server(debug=True)
